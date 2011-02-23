1653bf36b617257c0a8728b5653f419d34116f9f
Fargier Sylvain <sfargier@sunbeam.(none)>
2011-02-23 15:04:52 +0100

    Enhancing test_main.cc (when getopt is available)

--- src/cxx/tests/test_main.cc
+++ src/cxx/tests/test_main.cc
@@ -34,19 +34,80 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include <string>
+#include <list>
 #include <iostream>
 
+#include "config.h"
+
+#ifdef HAVE_GETOPT_LONG
+#include <getopt.h>
+#include <stdlib.h>
+
+static std::ostream &printTest(std::ostream &out, const CppUnit::Test *test) {
+  out << test->getName() << "\n";
+  for (unsigned int i = 0; i < test->getChildTestCount(); ++i)
+  {
+    printTest(out, test->getChildTestAt(i));
+  }
+  return out << std::flush;
+}
+
+#endif
+
 /* Test program */
-int main(int , char** argv)
+int main(int argc, char** argv)
 {
   CppUnit::TestResult testresult;
   CppUnit::TestResultCollector collectedresults;
   CppUnit::TestRunner testrunner;
   std::ofstream fb;
+  std::list< std::string > test_list;
+
+#ifdef HAVE_GETOPT_LONG
+  int c;
+  struct option long_options[] = {
+    {"help", 0, 0, 'h'},
+    {"list", 0, 0, 'l'},
+    {0, 0, 0, 0}
+  };
+
+  while ((c = getopt_long(argc, argv, "hl", long_options, NULL)) != -1) {
+    switch (c) {
+      case 'h':
+        std::cout << "Usage: " << argv[0] << " [test name]\n";
+        std::cout << "\nOptions:\n" <<
+          "  -h, --help           Show this help message and exit\n" <<
+          "  -l, --list           List available tests and exit" <<
+          std::endl;
+        exit(EXIT_SUCCESS);
+      case 'l':
+      {
+        CppUnit::Test *t = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+        std::cout << "Available test suites:\n";
+        printTest(std::cout, t);
+        delete t;
+        exit(EXIT_SUCCESS);
+      }
+    }
+  }
+
+  if (optind < argc) {
+    while (optind < argc) {
+      test_list.push_back(argv[optind++]);
+    }
+  }
+#endif
 
   testresult.addListener(&collectedresults);
   testrunner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
-  testrunner.run(testresult);
+  if (test_list.empty())
+    testrunner.run(testresult);
+  else {
+    std::list<std::string>::const_iterator it;
+    for (it = test_list.begin(); it != test_list.end(); ++it) {
+      testrunner.run(testresult, *it);
+    }
+  }
 
   fb.open((std::string(argv[0]) + ".xml").c_str());
   CppUnit::XmlOutputter xml_outputter(&collectedresults, fb);
