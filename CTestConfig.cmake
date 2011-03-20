f7c01af33c100971d85f5b55b480ca47e1276b8d
Fargier Sylvain <fargier.sylvain@free.fr>
2011-03-20 22:11:27 +0100

    Adding CTestConfig.cmake and .vproject files (vim and ctest)

---
+++ CTestConfig.cmake
@@ -0,0 +1,8 @@
+
+set(CTEST_PROJECT_NAME "misc-tools")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "minia.ath.cx")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=misc-tools")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
